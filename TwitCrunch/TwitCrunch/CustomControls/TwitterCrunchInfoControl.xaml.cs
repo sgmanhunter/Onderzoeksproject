37d232f3f281bc115caf9f1dbd2827bcb78084b9
savchenkoilya <savchenko.ilya92@gmail.com>
2013-11-16 15:48:52 +0100

    info control upgrade

--- TwitCrunch/TwitCrunch/CustomControls/TwitterCrunchInfoControl.xaml.cs
+++ TwitCrunch/TwitCrunch/CustomControls/TwitterCrunchInfoControl.xaml.cs
@@ -20,39 +20,9 @@ namespace TwitCrunch.CustomControls
     /// </summary>
     public partial class TwitterCrunchInfoControl : UserControl
     {
-
-        private string _searchKeyword;
-        private Grid grid;
-        private Accordion accordion;
-
-
-
         public TwitterCrunchInfoControl()
         {
             InitializeComponent();
-            InitializeThis();
-        }
-
-        private void InitializeThis()
-        {
-            //grid
-            grid = new Grid();
-            ColumnDefinition width1 = new ColumnDefinition();
-            width1.Width = new GridLength(137);
-            ColumnDefinition width2 = new ColumnDefinition();
-            width2.Width = new GridLength(663);
-
-            grid.ColumnDefinitions.Add(width1);
-            grid.ColumnDefinitions.Add(width2);
-
-            //accordion
-            accordion = new Accordion();
-
-            //add accordion to grid
-            grid.Children.Add(accordion);
-
-            //add grid to this controll
-            this.AddChild(grid);
         }
 
         public void addElementToAccordion(string element)
@@ -60,7 +30,7 @@ namespace TwitCrunch.CustomControls
             AccordionItem ai = new AccordionItem();
             ai.Header = element;
             ai.Content = element;
-            accordion.Items.Add(ai);
+            accTweets.Items.Add(ai);
         }
     }
 }
