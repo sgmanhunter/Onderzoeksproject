4fd29e86dd9b9d753831cd781c90fa6f669b690d
tsangsing <liblib@outlook.com>
2013-11-19 00:44:51 +0100

    libs toegevoegd en wat geprobeerd

--- TwitCrunch/TwitCrunch.data/TwitterCrunchStreaming.cs
+++ TwitCrunch/TwitCrunch.data/TwitterCrunchStreaming.cs
@@ -7,7 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Streaminvi;
-
+using TweetinCore;
+using TwitterToken;
+using Tweetinvi;
 
 namespace TwitCrunch.data
 {
@@ -15,8 +17,21 @@ namespace TwitCrunch.data
     {
         public void Stream()
         {
-           
 
+            // efkes hier verkeerd bezig zzzz 
+            TokenCreator i = new TokenCreator(ApplicationCredentials.Singleton.ConsumerKey, ApplicationCredentials.Singleton.ConsumerSecret);
+
+           // i.GenerateToken(ApplicationCredentials.Singleton.ConsumerKey);
+            
+
+            TokenSingleton.Token = token;
+
+            SimpleStream twitterStream = new SimpleStream("https://stream.twitter.com/1.1/statuses/sample.json");
+
+            twitterStream.StartStream(token,x => Console.WriteLine(x.Text));
+            
+            
+            
        
         }
     }
