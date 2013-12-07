dfd68999cee8d94a52c021330b1339f6a92c143d
savchenkoilya <savchenko.ilya92@gmail.com>
2013-12-07 17:26:04 +0100

    db

--- TwitCrunchStream/TwitCrunchStream/Tweet.cs
+++ TwitCrunchStream/TwitCrunchStream/Tweet.cs
@@ -4,20 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TweetinCore.Interfaces;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TwitCrunchStream
 {
     class Tweet
     {
         private string text;
-        private int id;
+        private string id;
         private string creatorUserName;
         private DateTime createdAt;
         private string tagString;
         public Tweet(ITweet tweet)
         {
             this.text = tweet.Text;
-            this.id = int.Parse(tweet.IdStr);
+            this.id = tweet.IdStr;
             this.createdAt = tweet.CreatedAt;
             this.creatorUserName = tweet.Creator.ScreenName;
             this.tagString = convertListToString(tweet.Hashtags);
@@ -46,7 +48,23 @@ namespace TwitCrunchStream
             Console.WriteLine("ID: " + this.id);
             Console.WriteLine("Hashtags: " + this.tagString);
 
-
+            try
+            {
+                System.Data.SqlClient.SqlConnection sqlConnection = new System.Data.SqlClient.SqlConnection(@"Server=Admin-PC\SQLEXPRESS;Database=twitcrunch;Trusted_Connection=True;");
+                sqlConnection.Open();
+                SqlCommand sc = new SqlCommand("INSERT INTO dbo.tweets VALUES (@id, @text, @username, @createdAt, @tags)", sqlConnection);
+                sc.Parameters.AddWithValue("@id", this.id);
+                sc.Parameters.AddWithValue("@text", this.text);
+                sc.Parameters.AddWithValue("@username", this.creatorUserName);
+                sc.Parameters.AddWithValue("@createdAt", this.createdAt);
+                sc.Parameters.AddWithValue("@tags", this.tagString);
+                sc.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                
+            }
         }
 
 
